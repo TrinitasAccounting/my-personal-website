f6588575d9fbfc924ba8dda0ec80c7389db6ce4d
Clay Mangum <clay@trinitasaccounting.com>
2024-01-12 12:09:24 -0600

    all text and img's are placed correctly

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 
-let h3 = document.createElement('h3');
-h3.textContent = "You are now subscribed to follow Clay Mangum on his Software Journey";
-document.querySelector("body").appendChild(h3);
+let h4 = document.createElement('h4');
+h4.textContent = "You are now subscribed to follow Clay Mangum on his Software Journey";
+document.querySelector("body").appendChild(h4);
 
+// function addH4 () {
+//     let h4 = document.createElement('h4');
+// }
 
 
 
